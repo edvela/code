1053066f886e6b3e86b0914e06cf782c2a9ad6d6
Eder <eder@gmail.com>
2019-08-26 21:32:03 -0500

    ajuste

---
+++ src/views/Services.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>titulo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus debitis odit! Possimus perspiciatis optio quis minus odit rem praesentium qui voluptatem molestias, molestiae totam vero, maiores quo repellendus aliquid!</p>
+    </div>
+</template>
